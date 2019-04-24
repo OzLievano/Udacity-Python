7b64f6ef4dcf20ce03685691e931dd2621e85c38
Osvaldo Lievano <Osvaldoalievano@gmail.com>
2019-04-23 23:13:44 -0700

    this commit creates a multi-color square

---
+++ order_matters.py
@@ -0,0 +1,19 @@
+# Import turtle module
+import turtle
+#Assign an object to method call
+ozzy = turtle.Turtle()
+#Assign first color line
+ozzy.color("red")
+ozzy.forward(100)
+ozzy.right(90)
+#Assign 2nd color line
+ozzy.color("orange")
+ozzy.forward(100)
+ozzy.right(90)
+#Assign 3rd color line
+ozzy.color("yellow")
+ozzy.forward(100)
+ozzy.right(90)
+#Assign 4th color line
+ozzy.color("green")
+ozzy.forward(100)
