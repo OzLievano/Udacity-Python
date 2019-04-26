1c6fb0becd5e6bf8cfb71ec2f4a3acc35e66dd52
Osvaldo Lievano <Osvaldoalievano@gmail.com>
2019-04-25 18:09:04 -0700

    this commit adds varible names to code

--- two.py
+++ two.py
@@ -1,7 +1,20 @@
 import turtle
 amy = turtle.Turtle()
-amy.color("blue")
+pretty_color="lightblue"
+amy.color(pretty_color)
 for side in [1,2,3,4]:
     amy.forward(100)
     amy.right(90)
-    
+
+
+george = turtle.Turtle()
+george.color(pretty_color)
+for side in [1,2,3,4,5]:
+    george.forward(100)
+    george.right(72)
+
+mary=turtle.Turtle()
+mary.color(pretty_color)
+for side in [1,2,3]:
+    mary.forward(100)
+    mary.right(30)
