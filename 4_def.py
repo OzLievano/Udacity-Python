f73475cf6b96fa63d43fb040b23fc2d892488900
Osvaldo Lievano <Osvaldoalievano@gmail.com>
2019-05-13 20:24:48 -0700

    This commit adds more parameters to the function

---
+++ 4_def.py
@@ -0,0 +1,10 @@
+import turtle
+
+def spiral(sides,turn,color,width):
+    t = turtle.Turtle()
+    t.color(color)
+    for n in range(sides):
+        t.forward(sides)
+        t.right(turn)
+
+spiral(100,45,"red",2)
